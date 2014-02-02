2b31cce97201f4e292d66bf922507db39e9a44c1
MohamadIsmail <mohammadismail.1993@gmail.com>
2014-02-02 20:33:46 +0200

    Update Game of sum.cpp

--- Level3/Game of sum.cpp
+++ Level3/Game of sum.cpp
@@ -1,31 +1,91 @@
-//#include<iostream>
-//#include<cmath>
-//#include<cstring>
-//using namespace std;
-//
-//int main()
-//{
-//	int size,arr[101][101],line[101] ,temp ,max_so_far, Max;
-//	while(cin>>size)
-//	{
-//		for(int i=0; i<size; i++)
-//		{
-//			for(int j=0; j<size; j++)
-//				cin>>arr[i][j];
-//		}
-//		temp=0,Max=1<<31;
-//		for(int k=0; k<size; k++)
-//		{
-//			temp+=line[k];
-//			Max=max(temp,Max);
-//			if(temp<0)
-//				temp=0;
-//		}
-//		for(int i=0; i<size; i++)
-//		{
-//			
-//		}
-//		cout<<max_so_far<<endl;
-//	}
-//	return 0;
-//}
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <queue>
+#include <assert.h>
+#include <cctype>
+#include <string.h>
+using namespace std;
+
+#define INF_MAX 2147483647
+#define INF_MIN -2147483647
+
+#define pi acos(-1.0)
+#define INF 1e9
+#define ll long long
+
+#define For(i, a, b) for( int i = (a); i < (b); i++ )
+#define Fors(i, sz) for( size_t i = 0; i < sz.size (); i++ )
+#define Fore(it, x) for(typeof (x.begin()) it = x.begin(); it != x.end (); it++)
+#define mem(a, s) memset(a, s, sizeof (a))
+#define Read() freopen("1.txt", "r", stdin)
+#define Write() freopen(".txt", "w", stdout)
+#define sz(c) ((int)(c).size())
+#define pb push_back
+#define mp make_pair
+#define all(v) v.begin(),v.end()
+#define vi vector<int>
+typedef pair<int,int> PAIR;
+typedef pair<PAIR ,int> PAIR2;
+string tostring(long long n){ostringstream ss; ss << n; return ss.str();}
+long long tonumber(string str){stringstream ss; ss << str; long long n; ss >>n; return n;}
+
+int dp[101][101],cum[101], arr[101];
+int sum(int x ,int y){
+	return cum[y]-cum[x]+arr[x];
+}
+
+int solve(int i, int j)
+{
+	if(i > j)
+		return 0;
+	int &res=dp[i][j];
+	if(res!=INF_MIN)
+		return res;
+	for (int k = i; k <= j; k++)
+		res=max(res,sum(i,k)-solve(k+1,j));
+	for (int k = j; k >=i ; k--)
+		res=max(res,sum(k,j)-solve(i,k-1));
+	return res;
+}
+int main()
+{
+	//Read();
+	int n;
+	while (cin >> n && n)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				dp[i][j]=dp[i][j]=INF_MIN;
+			}
+		}
+		for (int i = 0; i < n; i++)
+		{
+			cin >> arr[i];
+		}
+		cum[0]=arr[0];
+		for (int i = 1; i < n; i++)
+		{
+			cum[i]=cum[i-1]+arr[i];
+		}
+		cout << solve(0,n-1)<< endl;
+	}
+	return 0;
+}
