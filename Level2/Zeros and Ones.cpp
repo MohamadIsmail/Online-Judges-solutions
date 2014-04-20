abbca9d1f6b30dd7a4ad57c9422d9dc27f34971d
MohamadIsmail <mohammadismail.1993@gmail.com>
2014-04-20 21:07:05 +0200

    Update Zeros and Ones.cpp

--- Level2/Zeros and Ones.cpp
+++ Level2/Zeros and Ones.cpp
@@ -1,39 +1,39 @@
-//#include<iostream>
-//#include<string>
-//using namespace std;
-//int main()
-//{
-//
-//	string str;
-//	int x=1,m,l,n;
-//	while(cin>>str)
-//	{
-//	 cin>>n;
-//	 if(str=="0" && n==0)
-//	    break;
-//	 cout<<"Case "<<x<<":\n";
-//	 ++x;
-//	 for(int i=0;i<n;i++)
-//	 {
-//	 bool ok=true;
-//	 cin>>m>>l;
-//	 if(m>l)
-//	 swap(m,l);
-//	 char x=str[m];
-//	 for(int j=m+1;j<=l;j++)
-//	 {
-//	 if(str[j]!=x)
-//	 {
-//	 ok=false;
-//	 break;
-//	 }
-//	 }
-//	 if(ok)
-//	 cout<<"Yes\n";
-//	 else
-//	 cout<<"No\n";
-//	 }
-//
-//	}
-//	return 0;
-//}
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+
+	string str;
+	int x=1,m,l,n;
+	while(cin>>str)
+	{
+		cin>>n;
+		if(str=="0" && n==0)
+			break;
+		cout<<"Case "<<x<<":\n";
+		++x;
+		for(int i=0;i<n;i++)
+		{
+			bool ok=true;
+			cin>>m>>l;
+			if(m>l)
+				swap(m,l);
+			char x=str[m];
+			for(int j=m+1;j<=l;j++)
+			{
+				if(str[j]!=x)
+				{
+					ok=false;
+					break;
+				}
+			}
+			if(ok)
+				cout<<"Yes\n";
+			else
+				cout<<"No\n";
+		}
+
+	}
+	return 0;
+}
